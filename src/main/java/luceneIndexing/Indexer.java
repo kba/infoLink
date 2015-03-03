e0a375ad08425f3161d14a4aa871c6403bb6ecd0
Konstantin Baierer <konstantin.baierer@bib.uni-mannheim.de>
2015-03-03 12:43:27 +0100

    Implement OptionHandler pattern for Indexer, new option to forceOverwrite

--- src/main/java/luceneIndexing/Indexer.java
+++ src/main/java/luceneIndexing/Indexer.java
@@ -23,11 +23,15 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.store.FSDirectory;
 import java.io.File;
+import org.apache.commons.io.FileUtils;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Date;
 import java.util.HashMap;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 
 
 /** 
@@ -89,16 +93,24 @@ public class Indexer
 	 * 
 	 * @param fileMap	A map listing index output locations (keys) and input directories (values) containing the documents to index	
 	 */
-	public static void indexAllFiles(HashMap<File, File> fileMap)
+	public static void indexAllFiles(HashMap<File, File> fileMap, boolean forceOverwrite)
 	{
 		for (File indexDir : fileMap.keySet())
 		{
 			File docDir = fileMap.get(indexDir);
 
-			System.out.println("start");
+			System.out.println("start indexing");
 			if (indexDir.exists()) 
 			{
-				System.out.println("Cannot save index to '" + indexDir + "' directory, please delete it first");
+				if (forceOverwrite) {
+					System.out.println("Removing dir " + indexDir);
+					try {
+						FileUtils.deleteDirectory(indexDir);
+					} catch (IOException ioe) {
+						System.out.println("ERROR: Could not delete directory " + indexDir);
+					}
+				}
+				System.out.println("Cannot save index to '" + indexDir + "' directory, please delete it first or set --force");
 				continue;
 			}
 
@@ -118,11 +130,9 @@ public class Indexer
 			{
 				System.out.println(" caught a " + e.getClass() + "\n with message: " + e.getMessage());
 			}
-			System.out.println("end");
+			System.out.println("finished indexing");
 		}
 	}
-  
-	
 	/** 
 	 * Selects either all subdirectories in the specified directory (recursive mode) or only the root 
 	 * directory, specifies a path for the indexes to be created and starts the indexing process. 
@@ -130,20 +140,55 @@ public class Indexer
 	 * @param	args	args[0]: path to the corpus root directory; args[1]: path to the index directory; args[2]: "r" to set recursive mode
 	 */
 	public static void main(String[] args) {
-		if (args.length < 3) {
-			System.out.println("Usage: Indexer <corpusPath> <indexPath> <recursiveFlag>");
-			System.out.println("	corpusPath	path to the corpus root directory");
-			System.out.println("	indexPath	path to the index directory");
-			System.out.println("	<recursiveFlag>	\"r\" (without quotes) to select directories recursively [OPTIONAL]");
-			System.exit(1);
+		new OptionHandler().doMain(args);
+	}
+}
+
+/**
+* Class for processing command line options using args4j.
+*
+* @author konstantin.baierer@bib.uni-mannheim.de
+* @version 2014-03-03
+*/
+class OptionHandler {
+		
+	@Option(name="-c",usage="path of the corpus root directory", metaVar = "CORPUS_PATH", required = true)
+    private String root_corpus_str;
+		
+	@Option(name="-o",usage="path to save converted documents", metaVar = "OUTPUT_PATH", required = true)
+    private String root_index;
+		
+	@Option(name="-r",usage="Recursive indexing", metaVar = "RECURSIVE")
+    private boolean recursive = false;
+		
+	@Option(name="-f",usage="Force overwriting existing directories", metaVar = "FORCE")
+    private boolean forceOverwrite = false;
+
+	/**
+     * Parses all command line options and calls <emph>main</emph> method accordingly.
+     * 
+     * @param args			
+     * @throws IOException
+     */
+	public void doMain(String[] args)
+	{
+		CmdLineParser parser = new CmdLineParser(this); 
+
+        try { parser.parseArgument(args); } 
+        catch(CmdLineException e) {
+            System.err.println(e.getMessage());
+            System.err.println("Indexer [options...]");
+            parser.printUsage(System.err);
+            System.err.println();
+            return;
+        } catch (Exception e) {
+        	e.printStackTrace();
+        	return;
 		}
 		HashMap<File, File> toIndex = new HashMap<File, File>();
-		File root_corpus = new File(args[0]);
-		String root_index = new File (Paths.get(args[1]).normalize().toString()).getAbsolutePath();
-		boolean recursive;
-		try { recursive = args[2].toLowerCase().equals("r"); }
-		catch (ArrayIndexOutOfBoundsException e) { recursive = false; }
-		if (recursive) 
+		File root_corpus = new File(this.root_corpus_str);
+		String root_index = new File(Paths.get(this.root_index).normalize().toString()).getAbsolutePath();
+		if (this.recursive) 
 		{
 			for (File file : root_corpus.listFiles()) 
 			{
@@ -153,7 +198,6 @@ public class Indexer
 				}
 			}
 		}
-		else { toIndex.put(new File(root_index), root_corpus); }
-		indexAllFiles(toIndex);
+		Indexer.indexAllFiles(toIndex, this.forceOverwrite);
 	}
 }
