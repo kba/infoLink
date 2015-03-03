ef7e2260e1a043c6934d28f8bff48058170e9ae1
Konstantin Baierer <konstantin.baierer@bib.uni-mannheim.de>
2015-03-03 12:40:47 +0100

    fixed paths in the shell script

--- infoLinkCmd.sh
+++ infoLinkCmd.sh
@@ -12,24 +12,33 @@
 # -t = apply term search for dataset names listed in this file
 # first argument: classpath for InfoLink java classes (change if desired)
 
-PYTHON_SRC="src/main/python"
+BASE=$PWD
+PYTHON_SRC="$BASE/src/main/python"
+JAVA_CLASSPATH="$BASE/build/classes/main"
+echo $PYTHON_SRC
 
 # extract and clean text from pdf documents, remove bibliographies and learn and apply patterns, use uppercase_constraint
 # use ALLBUS, Allbus, Eurobarometer and ISSP as seeds
 python $PYTHON_SRC/infoLink.py \
-    -C "build/classes/main/" \
-    -e "../data/test/small_txt" \
-    -c "../data/test/small" \
-    -l "../data/test/train_small/" \
-    -o "../data/test/output_small" \
-    -s "\"ALLBUS Allbus Eurobarometer ISSP\"" \
-    -i "../data/test/Index_small" \
-    -m "../data/test/urnDict.csv" \
+    -C "$JAVA_CLASSPATH" \
+    -e "data/test/small_txt" \
+    -c "data/test/small" \
+    -l "data/test/train_small" \
+    -o "data/test/output_small" \
+    -s "'ALLBUS Allbus Eurobarometer ISSP'" \
+    -i "data/test/Index_small" \
+    -m "data/test/urnDict.csv" \
     -u \
-    .
+    $PYTHON_SRC
+# # learn patterns and apply patterns without text extraction. Use
+# uppercase_constraint 
+# python.exe # ../py/infoLink.py "."  #
+# -c "../data/test/small_txt" 
+# -l "../data/test/train_small/" # -s
+# "\"ALLBUS Allbus Eurobarometer ISSP\"" 
+# -i "../data/test/Index_small"
 
-# learn patterns and apply patterns without text extraction. Use uppercase_constraint
-# python.exe ../py/infoLink.py "." -c "../data/test/small_txt" -l "../data/test/train_small/" -s "\"ALLBUS Allbus Eurobarometer ISSP\"" -i "../data/test/Index_small" -o "../data/test/output_small" -m "../data/test/urnDict.csv" -u
+# -o "../data/test/output_small" # -m "../data/test/urnDict.csv" # -u
 
 # apply existing patterns and term search for known dataset names with prior text extraction. Use uppercase_constraint
 # python.exe ../py/infoLink.py "." -p "../data/test/patterns.csv" -o "../data/test/output_small" -e "../data/test/small_txt" -c "../data/test/small" -i "../data/test/Index_small" -t "../data/test/known_datasets.csv" -u
